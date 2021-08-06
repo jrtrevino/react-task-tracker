b1382b60654eedaf17913c7d3e6269874f8c3833
Joey <jrtrevin@calpoly.edu>
2021-08-05 20:16:56 -0700

    added linking to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
 import Button from './Button'
 import { useState } from "react";
+import { useLocation } from 'react-router-dom';
 
 
 const Header = ({ onDisplay }) => {
+    const location = useLocation()
     const [buttonText, setButtonText] = useState('Add')
     const [backgroundColor, setBackgroundColor] = useState('green')
     const onClick = () => {
@@ -13,7 +15,7 @@ const Header = ({ onDisplay }) => {
     return (
         <header className='header'>
             <h1>Task Tracker</h1>
-            <Button color={backgroundColor} text={buttonText} onClick={onClick}/>
+            {location.pathname === "/" && (<Button color={backgroundColor} text={buttonText} onClick={onClick}/>) }
         </header>
     )
 }
